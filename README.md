0e3b3f210e5f0c7ae7b27223127051f6e151ebd6
lenny <llaska@live.com>
2021-01-03 00:28:12 +1100

    all files updated and project put together, readme updated to reflect

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# HWWK6
+# HWWK6 - Weather Dashboard
+
+Tasked to create a weather dashboard that shows current weather for a location as well as a 5 day forecast, and collect search history and store locally.
+
+# Example
+The scheduler should desplay as shown: <br>
+![Imgur](https://i.imgur.com/EkfHkka.png)
+
+# Problems
+I had great difficulty piecing the seperate sections together to function as a whole. I sourced many different similar projects for ways to put it together, unfortunately I ran short of time and was unable to create the history tab without repeats and having a click functionality to bring up the 5 day forecast.
+Feeling like my knowledge is a bit lacking in putting together big projects in short time.
---
+++ script.js
@@ -0,0 +1,88 @@
+// Global Variables 
+var searchButton = $(".searchButton");
+var apiKey = "7e72908104bad6b6351598d22fe67f65";
+
+// Loop for local storage list of searched data
+for (var i = 0; i < localStorage.length; i++) {
+    var city = localStorage.getItem(i);
+    var cityName = $(".list-group").addClass("list-group-item");
+    cityName.append("<li>" + city + "</li>");
+}
+// Key count for local storage 
+var keyCount = 0;
+// Search button click event
+searchButton.click(function () {
+    var searchInput = $(".searchInput").val();
+    
+    // Variables for ajax info collection/search
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+
+
+    if (searchInput == "") {
+        console.log(searchInput);
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            var cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+            var local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount + 1;
+
+            // Create card to append data to 
+            var currentCard = $(".currentCard").append("<div>").addClass("card-body");
+            currentCard.empty();
+            var currentName = currentCard.append("<p>");
+            currentCard.append(currentName);
+
+            // Adjust returned date 
+            var timeUTC = new Date(response.dt * 1000);
+            // Append responses to card
+            currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+            var currentTemp = currentName.append("<p>");
+            currentName.append(currentTemp);
+            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+            
+            // UV Index URL
+            var uvIND = `https://api.openweathermap.org/data/2.5/uvi?&lat=${response.coord.lat}&lon=${response.coord.lon}&appid=7e72908104bad6b6351598d22fe67f65`;
+
+            // UV Index
+            $.ajax({
+                url: uvIND,
+                method: "GET"
+            }).then(function (response) {
+
+                var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
+                currentUV.addClass("UV");
+                currentTemp.append(currentUV);
+            });
+
+        });
+
+        // Call data for 5 day forcast 
+        $.ajax({
+            url: urlFiveDay,
+            method: "GET"
+        }).then(function (response) {
+            // Array for 5-days 
+            var day = [0, 8, 16, 24, 32];
+            var fiveDayCard = $(".fiveDayCard").addClass("card-body");
+            var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
+            fiveDayDiv.empty();
+            // For each for 5 days
+            day.forEach(function (i) {
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+
+                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+
+
+            })
+
+        });
+    }
+});
